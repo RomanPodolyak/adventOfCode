431f317cef6638a7bac57657ae956f9d730c4e8c
Román Podolyak <roman.podolyak.rpp@gmail.com>
2022-12-09 00:05:38 +0100

    Refactored 22/3A

--- 22/3/main.ts
+++ 22/3/main.ts
@@ -1,42 +1,52 @@
 import input from "./input";
 
+// first part
 type Bag = string[]
 type FullBag = [Bag, Bag]
 
+function getSameItemsFromLists<T>(lists: T[][]): T[] {
+    const intersection =
+        lists.reduce(
+            (prevList, currList) => prevList.filter(
+                item => currList.includes(item)
+            )
+        )
+    const uniqueIntersection = Array.from(new Set(intersection))
+    return uniqueIntersection
+}
+
+function getValue(item: string) {
+    const rawValue = item.charCodeAt(0) // A is 65, a is 97
+    const reducedValue = rawValue - 96 // A is -31, a is 1
+    const correctedValue =
+        reducedValue > 0 ? reducedValue : reducedValue + 58 // A is 27, a is 1
+    return correctedValue
+}
+
 // process input
 const rawFullBags = input.split('\n')
-const fullBags: FullBag[] = rawFullBags.map(rawFullBag => (rawFullBag.split('')))
+const bags: Bag[] = rawFullBags.map(rawFullBag => (rawFullBag.split('')))
 
 // get unique items per bag
-const fullBagLists = fullBags.map(
+const fullBagLists = bags.map(
     fullBag => {
         const pairOfBags: FullBag = [
             fullBag.slice(0, (fullBag.length / 2)),
             fullBag.slice(((fullBag.length / 2)), fullBag.length)
         ]
         const pairOfItemSets = pairOfBags.map(bag => new Set(bag))
-        const pairOfItemLists: FullBag = pairOfItemSets.map(bagSet => Array.from(bagSet))
+        const pairOfItemLists = pairOfItemSets.map(bagSet => Array.from(bagSet)) as FullBag
         return pairOfItemLists
     }
 )
 
 // get the same item in each fullBag
-const sameItems = fullBagLists.map(fullBag => {
-    const [leftBag, rightBag] = fullBag
-    const sameItemList = leftBag.filter((item) => rightBag.includes(item))
-    const sameItem = sameItemList[0]
-    return sameItem
-})
+const sameItems = fullBagLists.map(fullBag => getSameItemsFromLists(fullBag)[0])
 
 // get values
-const sameItemsValues = sameItems.map(item => {
-    const rawValue = item.charCodeAt(0); // A is 65, a is 97
-    const reducedValue = rawValue - 96; // A is -31, a is 1
-    const correctedValue =
-        reducedValue > 0 ? reducedValue : reducedValue + 58; // A is 27, a is 1
-    return correctedValue;
-});
+const sameItemsValues = sameItems.map(item => getValue(item[0]))
 
+// get total value
 const totalValue = sameItemsValues.reduce((prev, curr) => prev + curr)
 
 console.log(totalValue)
