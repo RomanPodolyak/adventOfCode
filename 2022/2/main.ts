57b084d737aa8951ebed0b69bd4e0c28a8ded490
Román Podolyak <roman.podolyak.rpp@gmail.com>
2022-12-08 14:22:29 +0100

    Refactored 2022/2A

--- 2022/2/main.ts
+++ 2022/2/main.ts
@@ -1,53 +1,65 @@
 import input from './input'
-type OpponentHand = 'A' | 'B' | 'C'
-type YourHand = 'X' | 'Y' | 'Z'
-type Round = [OpponentHand, YourHand]
-type RPS = 'rock' | 'paper' | 'sissors'
-type Result = 'win' | 'draw' | 'loss'
 
 // first part
+//types
+type OpponentCode = 'A' | 'B' | 'C'
+type YourCode = 'X' | 'Y' | 'Z'
+type Hand = 'rock' | 'paper' | 'sissors'
+type Result = 'win' | 'draw' | 'loss'
+type OpponentHandCode = OpponentCode
+type YourHandCode = YourCode
+type Round = [OpponentHandCode, YourHandCode]
+
 // prepare data
 const rawRounds = input.split('\n')
 const rounds = rawRounds.map(rawRound => rawRound.split(' ')) as Round[]
-const opponentHandNames: { [OpponentHand: string]: RPS } = {
+
+// dicts
+const opponentHands: { [key in OpponentHandCode]: Hand } = {
     'A': 'rock',
     'B': 'paper',
     'C': 'sissors'
 }
-const yourHandNames: { [YourHand: string]: RPS } = {
+const yourHands: { [key in YourHandCode]: Hand } = {
     'X': 'rock',
     'Y': 'paper',
     'Z': 'sissors'
 }
-const handValues: { [RPS: string]: number } = {
+const handValues: { [key in Hand]: number } = {
     'rock': 1,
     'paper': 2,
     'sissors': 3
 }
-const roundValues: { [Result: string]: number } = {
+const resultValues: { [key in Result]: number } = {
     'win': 6,
     'draw': 3,
     'loss': 0
 }
-const opponentHandToWin: { [RPS: string]: RPS } = {
+const opponentHandToWin: { [key in Hand]: Hand } = {
     'rock': 'sissors',
     'paper': 'rock',
     'sissors': 'paper'
 }
-function win(yourHand: YourHand, opponentHand: OpponentHand) {
-    return opponentHandToWin[yourHandNames[yourHand]] === opponentHandNames[opponentHand]
+
+// checks
+function isWin(yourHand: Hand, opponentHand: Hand) {
+    return opponentHandToWin[yourHand] === opponentHand
 }
-function draw(yourHand: YourHand, opponentHand: OpponentHand) {
-    return yourHandNames[yourHand] === opponentHandNames[opponentHand]
+
+function isDraw(yourHand: Hand, opponentHand: Hand) {
+    return yourHand === opponentHand
 }
+
+// calculation
 const results: number[] = []
 for (const round of rounds) {
-    const [opponentHand, yourHand] = round
-    const handValue: number = handValues[yourHandNames[yourHand]]
-    const roundResult = win(yourHand, opponentHand) ? 'win' : (draw(yourHand, opponentHand) ? 'draw' : 'loss')
-    const roundValue = roundValues[roundResult]
-    const resultValue = handValue + roundValue
-    results.push(resultValue)
+    const [opponentHandCode, yourHandCode] = round
+    const [opponentHand, yourHand] = [opponentHands[opponentHandCode], yourHands[yourHandCode]]
+    const roundResult = isWin(yourHand, opponentHand) ? 'win' : (isDraw(yourHand, opponentHand) ? 'draw' : 'loss')
+    const resultValue = resultValues[roundResult]
+    const handValue = handValues[yourHands[yourHandCode]]
+    const totalValue = resultValue + handValue
+    results.push(totalValue)
 }
 const total = results.reduce((prev, curr) => prev + curr)
 console.log(total);
